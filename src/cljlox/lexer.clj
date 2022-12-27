6678c7cd183e868657def079430d8126e8471dbb
Ketan Srivastav <ketan.srivastav@gmail.com>
2022-12-27 14:24:22 +0530

    lookahead

--- src/cljlox/lexer.clj
+++ src/cljlox/lexer.clj
@@ -1,5 +1,19 @@
 (ns celjlox.lexer)
 
+(def allowed-tokens ["!" "="])
+(def allowed-tokens-2-chars ["!!" "==" "="])
+
+(defn get-until
+  [predicate source position]
+  (loop [current position lexeme ""]
+    (if (and (<= (inc current) (count source)) (predicate (nth source current)))
+      (recur (inc current) (str lexeme (str (nth source current))))
+      lexeme)))
+
+(defn lex->token [token position]
+  {:token token
+   :postiion position})
+
 (defn scan
   [source]
   ((fn scan*
@@ -7,11 +21,25 @@
      "recursivly lex one character -> token at a time"
      (if (>= character-pointer (count source))
        [tokens errors]
-       (let [ch (nth source character-pointer)]
-         (print ":" ch)
-         (recur (inc character-pointer) [tokens errors]))))
-   0 [[:token] [:error]]))
+       (let [ch (str (nth source character-pointer))]
+         ;; check for 1 char 
+         (if (some #(= % ch) allowed-tokens)
+           (recur (+ character-pointer (count ch)) [(conj tokens lex->token) errors])
 
-(scan "hello
-helo
-")
+           (if (some #(= % ch) allowed-tokens-2-chars)
+             (recur (+ character-pointer (count ch)) [(conj tokens lex->token) errors])
+
+             (recur (inc character-pointer) [tokens errors]))))))
+   0 [[] []]))
+
+#_(scan "hell=o
+            open ==0
+            hel =o ")
+(get-until (fn [ch]
+             (print "->" ch)
+             (-> ch
+                 (str)
+
+                 (read-string)
+                 (number?))) "0011288=3" 0)
+
