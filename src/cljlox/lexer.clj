1eaf793a3126b2190df33193b0844ca138a135e8
Ketan Srivastav <ketan.srivastav@gmail.com>
2022-12-27 20:30:14 +0530

    lexer logic

--- src/cljlox/lexer.clj
+++ src/cljlox/lexer.clj
@@ -1,7 +1,10 @@
 (ns celjlox.lexer)
 
-(def allowed-tokens ["!" "="])
-(def allowed-tokens-2-chars ["=="])
+(def allowed-tokens [">" "<" " ="])
+(def allowed-tokens-2-chars ["==" "!="])
+
+(defn- digit? [ch]
+  (not (nil? (re-matches #"\d" ch))))
 
 (defn get-until
   [predicate source position]
@@ -22,25 +25,30 @@
      "recursivly lex one character -> token at a time"
      (if (>= character-pointer (count source))
        [tokens errors]
-
-       (let [ch (str (nth source character-pointer))]
+       (let [ch (str (nth source character-pointer))
+             can-look-ahead? (< (inc character-pointer) (count source))
+             ch2 (if can-look-ahead? (str ch (str (nth source (inc character-pointer)))))]
          ;; check for 2 length char 
-         (if (and (< (inc character-pointer) (count source)) (some #(= % (str ch (str (nth source (inc character-pointer))))) allowed-tokens-2-chars))
-           (recur (+ character-pointer 2) [(conj tokens (lex->token (str ch (nth source (inc character-pointer))) character-pointer)) errors])
-
+         (if (and can-look-ahead? (some #(= % ch2) allowed-tokens-2-chars))
+           (recur (+ character-pointer 2) [(conj tokens (lex->token ch2 character-pointer)) errors])
            (do
-             ;; check for 1 length char 
-             (if (some #(= % ch) allowed-tokens)
-
-               (recur (+ character-pointer (count ch)) [(conj tokens (lex->token ch character-pointer)) errors])
+             ;; check for string literal
+             (if (and can-look-ahead? (= ch "\""))
+               (let [string (get-until #(not= % "\"") source (inc character-pointer))]
+                 (recur (+ 2 character-pointer (count string)) [(conj tokens (lex->token string character-pointer)) errors]))
                (do
-                 ;; check for literals : numbers     
-                 (if (digit? ch)
-                   (let [literal (get-until digit? source character-pointer)]
-                     (recur (+ character-pointer (count literal)) [(conj tokens (lex->token literal character-pointer)) errors]))
-                   ;; no matchinng clause
-                   (recur (inc character-pointer) [tokens errors])))))))))
+                 ;; check for 1 length char 
+                 (if (some #(= % ch) allowed-tokens)
+
+                   (recur (+ character-pointer (count ch)) [(conj tokens (lex->token ch character-pointer)) errors])
+                   (do
+                     ;; check for literals : numbers     
+                     (if (digit? ch)
+                       (let [literal (get-until digit? source character-pointer)]
+                         (recur (+ character-pointer (count literal)) [(conj tokens (lex->token literal character-pointer)) errors]))
+                       ;; identifers
+                       ;;(if)
+                       ;; no matchinng clause
+                       (recur (inc character-pointer) [tokens errors])))))))))))
    0 [[] []]))
-(defn- digit? [ch]
-  (not (nil? (re-matches #"\d" ch))))
-(scan "hell=903 open==0hel=o3")
+(scan "hell=903 open==0hel=o3 \"ok\" 30=P87")
