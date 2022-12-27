eaf8b5bd1428bacb6a2475102b3e72f593796df8
Ketan Srivastav <ketan.srivastav@gmail.com>
2022-12-27 17:53:00 +0530

    lexer logic

--- src/cljlox/lexer.clj
+++ src/cljlox/lexer.clj
@@ -1,16 +1,17 @@
 (ns celjlox.lexer)
 
 (def allowed-tokens ["!" "="])
-(def allowed-tokens-2-chars ["!!" "==" "="])
+(def allowed-tokens-2-chars ["=="])
 
 (defn get-until
   [predicate source position]
   (loop [current position lexeme ""]
-    (if (and (<= (inc current) (count source)) (predicate (nth source current)))
+    (if (and (<= (inc current) (count source)) (predicate (str (nth source current))))
       (recur (inc current) (str lexeme (str (nth source current))))
       lexeme)))
 
-(defn lex->token [token position]
+(defn lex->token
+  [token position]
   {:token token
    :postiion position})
 
@@ -21,25 +22,25 @@
      "recursivly lex one character -> token at a time"
      (if (>= character-pointer (count source))
        [tokens errors]
+
        (let [ch (str (nth source character-pointer))]
-         ;; check for 1 char 
-         (if (some #(= % ch) allowed-tokens)
-           (recur (+ character-pointer (count ch)) [(conj tokens lex->token) errors])
+         ;; check for 2 length char 
+         (if (and (< (inc character-pointer) (count source)) (some #(= % (str ch (str (nth source (inc character-pointer))))) allowed-tokens-2-chars))
+           (recur (+ character-pointer 2) [(conj tokens (lex->token (str ch (nth source (inc character-pointer))) character-pointer)) errors])
 
-           (if (some #(= % ch) allowed-tokens-2-chars)
-             (recur (+ character-pointer (count ch)) [(conj tokens lex->token) errors])
+           (do
+             ;; check for 1 length char 
+             (if (some #(= % ch) allowed-tokens)
 
-             (recur (inc character-pointer) [tokens errors]))))))
+               (recur (+ character-pointer (count ch)) [(conj tokens (lex->token ch character-pointer)) errors])
+               (do
+                 ;; check for literals : numbers     
+                 (if (digit? ch)
+                   (let [literal (get-until digit? source character-pointer)]
+                     (recur (+ character-pointer (count literal)) [(conj tokens (lex->token literal character-pointer)) errors]))
+                   ;; no matchinng clause
+                   (recur (inc character-pointer) [tokens errors])))))))))
    0 [[] []]))
-
-#_(scan "hell=o
-            open ==0
-            hel =o ")
-(get-until (fn [ch]
-             (print "->" ch)
-             (-> ch
-                 (str)
-
-                 (read-string)
-                 (number?))) "0011288=3" 0)
-
+(defn- digit? [ch]
+  (not (nil? (re-matches #"\d" ch))))
+(scan "hell=903 open==0hel=o3")
