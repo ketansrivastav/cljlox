515f289eeb366ca759534e528ef8a1f2bf6b65f8
Ketan Srivastav <ketan.srivastav@gmail.com>
2022-12-29 22:47:39 +0530

    lexing all

--- src/cljlox/lexer.clj
+++ src/cljlox/lexer.clj
@@ -1,8 +1,37 @@
 (ns celjlox.lexer)
-
-(def allowed-tokens [">" "<" "=" "+" "-"])
-(def allowed-tokens-2-chars ["==" "!=" "<=" ">="])
-
+(def vocabulary [{:token "<"
+                  :type ::LESS-THAN}
+                 {:token ">"
+                  :type ::GREATER-THAN}
+                 {:token "="
+                  :type ::EQUAL}
+                 {:token "+"
+                  :type ::ADD}
+                 {:token "-"
+                  :type ::MINUS}
+                 {:token "=="
+                  :type ::EQUALS-TO}
+                 {:keyword "and"
+                  :type ::AND}
+                 {:keyword "class"
+                  :type ::class}])
+(def allowed-keywords
+  #{"and"
+    "class"
+    "else"
+    "false"
+    "for"
+    "fun"
+    "if"
+    "nil"
+    "or"
+    "print"
+    "return"
+    "super"
+    "this"
+    "true"
+    "var"
+    "while"})
 (defn- digit? [ch]
   (not (nil? (re-matches #"\d" ch))))
 
@@ -17,9 +46,13 @@
       lexeme)))
 
 (defn lex->token
-  [token position]
-  {:token token
-   :postiion position})
+  ([token-type position]
+   (lex->token token-type nil position))
+  (
+   [token-type literal position]
+   {:token-type token-type
+    :literal literal
+    :postiion position}))
 
 (defn scan
   [source]
@@ -30,31 +63,49 @@
        [tokens errors]
        (let [ch (str (nth source character-pointer))
              can-look-ahead? (< (inc character-pointer) (count source))
-             ch2 (if can-look-ahead? (str ch (str (nth source (inc character-pointer)))))]
+             ch2 (if can-look-ahead? (str ch (str (nth source (inc character-pointer)))))
+             allowed-tokens-1-ch
+               (filter #(and (contains? % :token) (= (count (:token %)) 1)) vocabulary)
+             allowed-tokens-2-ch
+               (filter #(and (contains? % :token) (= (count (:token %)) 2)) vocabulary)]
          ;; check for 2 length char 
-         (if (and can-look-ahead? (some #(= % ch2) allowed-tokens-2-chars))
-           (recur (+ character-pointer 2) [(conj tokens (lex->token ch2 character-pointer)) errors])
+         (if (and can-look-ahead? (some #(= % ch2) (map :token allowed-tokens-2-ch)))
+           (let [token (->> allowed-tokens-2-ch
+                            (filter #(= (:token %) ch2))
+                            (first)
+                            (:type))]
+             (recur (+ character-pointer (count ch2)) [(conj tokens (lex->token token character-pointer)) errors]))
+
            (do
              ;; check for string literal
              (if (and can-look-ahead? (= ch "\""))
                (let [string (get-until #(not= % "\"") source (inc character-pointer))]
-                 (recur (+ 2 character-pointer (count string)) [(conj tokens (lex->token string character-pointer)) errors]))
+                 (recur (+ 2 character-pointer (count string)) [(conj tokens (lex->token ::STRING string character-pointer)) errors]))
                (do
                  ;; check for 1 length char 
-                 (if (some #(= % ch) allowed-tokens)
-                   (recur (+ character-pointer (count ch)) [(conj tokens (lex->token ch character-pointer)) errors])
+                 (if (some #(= % ch) (map :token allowed-tokens-1-ch))
+                   (let [token (->> allowed-tokens-1-ch
+                                    (filter #(= (:token %) ch))
+                                    (first)
+                                    (:type))]
+                     (recur (+ character-pointer (count ch)) [(conj tokens (lex->token token character-pointer)) errors]))
                    (do
                      ;; check for literals : numbers     
                      (if (digit? ch)
                        (let [literal (get-until digit? source character-pointer)]
-                         (recur (+ character-pointer (count literal)) [(conj tokens (lex->token literal character-pointer)) errors]))
-                       ;; identifers
+                         (recur (+ character-pointer (count literal)) [(conj tokens (lex->token ::NUMBER literal character-pointer)) errors]))
+                       ;; identifers and keywords;
                        (if (identifier-characters? ch)
-                         (let [identifier (get-until #(or (digit? %1) (identifier-characters? %1)) source character-pointer)]
+                         (let [identifier (get-until #(or (digit? %1) (identifier-characters? %1)) source character-pointer)
+                               is-keyword? (some #(= (:keyword %1) identifier) (map :keyword vocabulary))
+                               keywords (map :keyword vocabulary)]
 
-                           (recur (+ character-pointer (count identifier)) [(conj tokens (lex->token identifier character-pointer)) errors]))
+                           (recur (+ character-pointer (count identifier)) [(conj tokens
+                                                                                  (if is-keyword?
+                                                                                    (lex->token ::KEYWORD (get keywords identifier) character-pointer)
+                                                                                    (lex->token ::IDENTIFIER identifier character-pointer))) errors]))
                          ;; no matchinng clause
                          (recur (inc character-pointer) [tokens errors]))))))))))))
    0 [[] []]))
-(scan "hell=903 open==0hel=o3 \"ok\" 30=P87")
-(identifier-characters? "3")
+(scan "hell=903 open==0hel=o3 \"ok\" 30=P87 if ifr then thenedn")
+
